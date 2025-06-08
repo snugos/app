be802a05796367ec0656db2fd407976956063886
snugos <snugsworth@gmail.com>
2025-06-07 20:31:37 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -88,15 +88,14 @@ function applyUserTheme() {
     }
 }
 
+// --- Start of Corrected Code ---
 function handleMasterEffectsUIUpdate() {
     const rackWindow = getWindowByIdState('masterEffectsRack');
     if (rackWindow && rackWindow.element && !rackWindow.isMinimized) {
-        const listDiv = rackWindow.element.querySelector('#effectsList-master');
-        const controlsContainer = rackWindow.element.querySelector('#effectControlsContainer-master');
-        const masterEffects = getMasterEffectsState();
-        renderEffectsList(masterEffects, 'master', listDiv, controlsContainer);
+        rackWindow.refresh();
     }
 }
+// --- End of Corrected Code ---
 
 function handleTrackUIUpdate(trackId, reason, detail) {
     const track = getTrackByIdState(trackId);
@@ -143,19 +142,10 @@ function handleTrackUIUpdate(trackId, reason, detail) {
         }
     }
 
-    // --- Start of Corrected Code ---
     if (reason === 'effectsChanged') {
-        console.log(`[EFFECTS_DEBUG_6] handleTrackUIUpdate received 'effectsChanged' for track ${trackId}.`);
         const rackWindow = getWindowByIdState(`effectsRack-${trackId}`);
-        if (rackWindow && rackWindow.element && !rackWindow.isMinimized) {
-            const listDiv = rackWindow.element.querySelector(`#effectsList-${trackId}`);
-            const controlsContainer = rackWindow.element.querySelector(`#effectControlsContainer-${trackId}`);
-            renderEffectsList(track, 'track', listDiv, controlsContainer);
-        } else {
-            console.warn(`[EFFECTS_DEBUG_6] Could not find or update effects rack window for track ${trackId}.`);
-        }
+        rackWindow?.refresh();
     }
-    // --- End of Corrected Code ---
     
     if (reason === 'nameChanged' || reason === 'clipsChanged') {
         renderTimeline();
