22209b361aa228506c820b998c543b7825e08b6c
snugos <snugsworth@gmail.com>
2025-06-10 15:25:50 -0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,25 +1,25 @@
 // js/ui.js - Main UI Orchestrator
 
 // Import initializers from all UI sub-modules
-import { initializeInspectorUI } from './ui/inspectorUI.js'; //
-import { initializeMixerUI } from './ui/mixerUI.js'; //
-import { initializeEffectsRackUI } from './ui/effectsRackUI.js'; //
-import { initializeTimelineUI } from './ui/timelineUI.js'; //
-import { initializeSoundBrowserUI } from './ui/soundBrowserUI.js'; //
-import { initializePianoRollUI } from './ui/pianoRollUI.js'; //
-import { initializeYouTubeImporterUI } from './ui/youtubeImporterUI.js'; //
+import { initializeInspectorUI } from './ui/inspectorUI.js';
+import { initializeMixerUI } from './ui/mixerUI.js';
+import { initializeEffectsRackUI } from './ui/effectsRackUI.js';
+import { initializeTimelineUI } from './ui/timelineUI.js';
+import { initializeSoundBrowserUI } from './ui/soundBrowserUI.js';
+import { initializePianoRollUI } from './ui/pianoRollUI.js';
+import { initializeYouTubeImporterUI } from './ui/youtubeImporterUI.js';
 // NEW: Import the profile UI functions
 import { initializeProfileUI, openProfileWindow } from './ui/profileUI.js';
 
 // Import all exported functions from the sub-modules that main.js needs
-import { createKnob } from './ui/knobUI.js'; //
-import { openTrackInspectorWindow, renderSamplePads, updateSliceEditorUI, renderDrumSamplerPads, updateDrumPadControlsUI } from './ui/inspectorUI.js'; //
-import { openMixerWindow, updateMixerWindow } from './ui/mixerUI.js'; //
-import { openTrackEffectsRackWindow, openMasterEffectsRackWindow, renderEffectsList, renderEffectControls } from './ui/effectsRackUI.js'; //
-import { openTimelineWindow, renderTimeline, updatePlayheadPosition } from './ui/timelineUI.js'; //
-import { openSoundBrowserWindow, renderSoundBrowser, renderDirectoryView } from './ui/soundBrowserUI.js'; //
-import { openPianoRollWindow, updatePianoRollPlayhead } from './ui/pianoRollUI.js'; //
-import { openYouTubeImporterWindow } from './ui/youtubeImporterUI.js'; //
+import { createKnob } from './ui/knobUI.js';
+import { openTrackInspectorWindow, renderSamplePads, updateSliceEditorUI, renderDrumSamplerPads, updateDrumPadControlsUI } from './ui/inspectorUI.js';
+import { openMixerWindow, updateMixerWindow } from './ui/mixerUI.js';
+import { openTrackEffectsRackWindow, openMasterEffectsRackWindow, renderEffectsList, renderEffectControls } from './ui/effectsRackUI.js';
+import { openTimelineWindow, renderTimeline, updatePlayheadPosition } from './ui/timelineUI.js';
+import { openSoundBrowserWindow, renderSoundBrowser, renderDirectoryView } from './ui/soundBrowserUI.js';
+import { openPianoRollWindow, updatePianoRollPlayhead } from './ui/pianoRollUI.js';
+import { openYouTubeImporterWindow } from './ui/youtubeImporterUI.js';
 
 /**
  * Initializes all UI sub-modules by passing them the appServices object.
@@ -28,16 +28,16 @@ import { openYouTubeImporterWindow } from './ui/youtubeImporterUI.js'; //
  */
 export function initializeUIModule(appServices) {
     // Make createKnob available as a service for other modules to use
-    appServices.createKnob = (opts) => createKnob(opts, appServices); //
+    appServices.createKnob = (opts) => createKnob(opts, appServices);
     
     // Initialize all modules
-    initializeInspectorUI(appServices); //
-    initializeMixerUI(appServices); //
-    initializeEffectsRackUI(appServices); //
-    initializeTimelineUI(appServices); //
-    initializeSoundBrowserUI(appServices); //
-    initializePianoRollUI(appServices); //
-    initializeYouTubeImporterUI(appServices); //
+    initializeInspectorUI(appServices);
+    initializeMixerUI(appServices);
+    initializeEffectsRackUI(appServices);
+    initializeTimelineUI(appServices);
+    initializeSoundBrowserUI(appServices);
+    initializePianoRollUI(appServices);
+    initializeYouTubeImporterUI(appServices);
     // NEW: Initialize the profile UI module
     initializeProfileUI(appServices);
 }
@@ -45,27 +45,27 @@ export function initializeUIModule(appServices) {
 // Export all the functions that main.js needs to build the appServices object.
 // This file acts as a single entry point for all UI functionality.
 export {
-    createKnob, //
-    openTrackInspectorWindow, //
-    openMixerWindow, //
-    updateMixerWindow, //
-    openTrackEffectsRackWindow, //
-    openMasterEffectsRackWindow, //
-    renderEffectsList, //
-    renderEffectControls, //
-    openTimelineWindow, //
-    renderTimeline, //
-    updatePlayheadPosition, //
-    openSoundBrowserWindow, //
-    renderSoundBrowser, //
-    renderDirectoryView, //
-    openPianoRollWindow, //
-    updatePianoRollPlayhead, //
-    openYouTubeImporterWindow, //
-    renderSamplePads, //
-    updateSliceEditorUI, //
-    renderDrumSamplerPads, //
-    updateDrumPadControlsUI, //
+    createKnob,
+    openTrackInspectorWindow,
+    openMixerWindow,
+    updateMixerWindow,
+    openTrackEffectsRackWindow,
+    openMasterEffectsRackWindow,
+    renderEffectsList,
+    renderEffectControls,
+    openTimelineWindow,
+    renderTimeline,
+    updatePlayheadPosition,
+    openSoundBrowserWindow,
+    renderSoundBrowser,
+    renderDirectoryView,
+    openPianoRollWindow,
+    updatePianoRollPlayhead,
+    openYouTubeImporterWindow,
+    renderSamplePads,
+    updateSliceEditorUI,
+    renderDrumSamplerPads,
+    updateDrumPadControlsUI,
     // NEW: Add openProfileWindow to the exports
     openProfileWindow,
 };
