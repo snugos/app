1fc27bae0c414bd81f41f3159812f64f50eab9e5
snugos <snugsworth@gmail.com>
2025-06-12 05:18:37 -0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -4,7 +4,7 @@
 // import * as Constants from './constants.js';
 // import { showNotification, showCustomModal } from './utils.js';
 
-import { showNotification, showCustomModal } from './daw/utils.js'; // CORRECTED PATH
+import { showNotification, showCustomModal } from './daw/utils.js';
 
 let localAppServices = {};
 let loggedInUser = null;
