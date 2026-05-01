bd4033de2bf7e78b12e38e7455cc7af2c4b45309
zocomputer <help@zocomputer.com>
2026-05-01 05:44:17 +0000

    Fix: Duplicate if block syntax error in handleOpenSequencer

--- js/main.js
+++ js/main.js
@@ -496,8 +496,6 @@ const appServices = {
 
     handleOpenSequencer: (trackId) => {
         if (typeof openTrackSequencerWindow === 'function') openTrackSequencerWindow(trackId);
-    }
-        if (typeof openTrackSequencerWindow === 'function') openTrackSequencerWindow(trackId);
     },
 
     handleTimelineLaneDrop,
